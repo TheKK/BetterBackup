b7cda586fa479885c1ff72e07f14998245f2df94
yingrueil <yingrueil@synology.com>
2023-06-28 13:42:16 +0800

    [exe] introduce module, LocalCache

---
+++ app/LocalCache.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE Strict #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module LocalCache
+  ( initialize
+  ) where
+
+import Control.Monad (when, unless)
+
+import qualified Data.Text.IO as T
+
+import Path (Path, Abs, Rel, File, Dir, (</>), relfile)
+import qualified Path as Path
+
+import qualified System.Directory as D
+
+import qualified UnliftIO as Un
+
+import Config(Config(..))
+import qualified Config
+
+initialize :: Path.SomeBase Dir -> Config -> IO ()
+initialize cache_p config = do
+    repo_path <- render_to_abs_dir cache_p
+
+    repo_exists <- D.doesDirectoryExist $ Path.toFilePath repo_path
+    unless repo_exists $ do
+      Un.throwString $ "path '" <> Path.toFilePath repo_path <> "' does not exist, please construct it properly first" 
+
+    let config_path = Path.toFilePath $ repo_path </> config_filename
+    config_exists <- D.doesFileExist config_path
+    when config_exists $ do
+      Un.throwString $ "config file '" <> config_path <> "' has already existed" 
+
+    initialize_config repo_path config
+
+initialize_config :: Path Abs Dir -> Config -> IO ()
+initialize_config repo_path = T.writeFile (Path.toFilePath $ repo_path </> config_filename) . Config.renderConfig
+
+config_filename :: Path Rel File
+config_filename = [relfile|config.toml|]
+
+render_to_abs_dir :: Path.SomeBase Dir -> IO (Path Abs Dir)
+render_to_abs_dir = \case
+  Path.Abs p -> pure p
+  Path.Rel p -> do
+    cwd <- Path.parseAbsDir =<< D.getCurrentDirectory
+    pure $ cwd </> p
