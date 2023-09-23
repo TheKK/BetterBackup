d9e0acb3337f92a49e65f456e2e5d05d9979c979
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2023-09-23 16:12:13 +0800

    [lib] inline readStatistics & modifyStatistic'

--- src/Better/Statistics/Backup.hs
+++ src/Better/Statistics/Backup.hs
@@ -12,11 +12,13 @@ import qualified UnliftIO as Un
 import qualified Better.Statistics.Backup.Class as Base
 
 -- TODO Maybe could move to Better.Statistics
-readStatistics :: (Base.MonadBackupStat m, Un.MonadUnliftIO m) => m (Un.TVar a) -> m a
+{-# INLINE readStatistics #-}
+readStatistics :: (Un.MonadUnliftIO m) => m (Un.TVar a) -> m a
 readStatistics mtvar = mtvar >>= Un.readTVarIO
 
 -- TODO Maybe could move to Better.Statistics
-modifyStatistic' :: (Base.MonadBackupStat m, Un.MonadUnliftIO m) => m (Un.TVar a) -> (a -> a) -> m ()
+{-# INLINE modifyStatistic' #-}
+modifyStatistic' :: (Un.MonadUnliftIO m) => m (Un.TVar a) -> (a -> a) -> m ()
 modifyStatistic' mtvar f = do
   tvar <- mtvar
   Un.atomically $ Un.modifyTVar' tvar f
