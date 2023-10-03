592be4175847d95f89dc57cdf1d5b64f96acb1eb
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2023-10-03 13:25:33 +0800

    [lib] refactor TmpDir for inlining, scope of IO and imports

--- src/Better/TempDir.hs
+++ src/Better/TempDir.hs
@@ -1,54 +1,54 @@
-{-# LANGUAGE Strict #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE UndecidableInstances #-}
 
-module Better.TempDir
-  ( MonadTmp(..)
-  , TheMonadTmp(..)
-  ) where
+module Better.TempDir (
+  MonadTmp (..),
+  TheMonadTmp (..),
+) where
 
-
-import Control.Monad
-import Control.Monad.IO.Class
-import Control.Monad.Catch (MonadThrow, MonadCatch, MonadMask)
-
-import UnliftIO
-import UnliftIO.Directory
-import UnliftIO.Exception
+import Control.Monad.Catch (MonadCatch, MonadMask, MonadThrow)
+import Control.Monad.IO.Class (MonadIO)
 
 import qualified Capability.Reader as C
 
 import qualified System.Posix.Temp as P
 
-import qualified Path
 import Path (Path, (</>))
+import qualified Path
 
-import Data.Coerce (coerce)
-
-import Better.TempDir.Class (MonadTmp(..))
+import Better.TempDir.Class (MonadTmp (..))
+import Control.Exception (bracketOnError)
+import qualified Control.Monad.IO.Unlift as Un
+import System.Directory (removeFile)
+import System.IO (hClose)
 
 newtype TheMonadTmp m a = TheMonadTmp (m a)
-  deriving (Functor, Applicative, Monad, MonadIO, MonadThrow, MonadCatch, MonadMask, MonadUnliftIO)
+  deriving (Functor, Applicative, Monad, MonadIO, MonadThrow, MonadCatch, MonadMask, Un.MonadUnliftIO)
 
 runTheMonadTmp :: TheMonadTmp m a -> m a
 runTheMonadTmp (TheMonadTmp m) = m
 {-# INLINE runTheMonadTmp #-}
 
-instance (C.HasReader "tmp_dir" (Path Path.Abs Path.Dir) m, MonadUnliftIO m) => MonadTmp (TheMonadTmp m) where
+instance (C.HasReader "tmp_dir" (Path Path.Abs Path.Dir) m, Un.MonadUnliftIO m) => MonadTmp (TheMonadTmp m) where
+  {-# INLINE withEmptyTmpFile #-}
   withEmptyTmpFile run = TheMonadTmp $ do
     tmp_dir <- C.ask @"tmp_dir"
-    let p = tmp_dir </> [Path.relfile|file-|]
+    Un.withRunInIO $ \unlift -> do
+      let p = tmp_dir </> [Path.relfile|file-|]
 
-    bracketOnError
-      (do
-         (filename, fd) <- liftIO $ P.mkstemp $ Path.fromAbsFile p
-         hClose fd
-         pure filename
-      )
-      removeFile
-      ((runTheMonadTmp . run) <=< (liftIO . Path.parseAbsFile))
+      bracketOnError
+        ( do
+            (filename, fd) <- P.mkstemp $ Path.fromAbsFile p
+            hClose fd
+            pure filename
+        )
+        removeFile
+        ( \filename -> do
+            abs_file <- Path.parseAbsFile filename
+            unlift $ runTheMonadTmp $ run abs_file
+        )
