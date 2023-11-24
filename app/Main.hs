077a0009e0cd62d142ee85ce950ebb579ae7b2e3
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2023-11-24 17:50:31 +0800

    [exe] now fits text of cli help to terminal when possible

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE Strict #-}
 
-module Main
-  ( main
-  ) where
+module Main (
+  main,
+) where
 
 import qualified Options.Applicative as O
-import Control.Monad (join)
+import qualified System.Console.Terminal.Size as Term
+
 import qualified Cli
+import Control.Monad (join)
 
 main :: IO ()
-main = join $ O.customExecParser (O.prefs $ O.showHelpOnError <> O.showHelpOnEmpty) Cli.cmds
+main = do
+  term_column_perf <- maybe mempty (O.columns . Term.width) <$> Term.size
+  join $ O.customExecParser (O.prefs $ term_column_perf <> O.showHelpOnError <> O.showHelpOnEmpty) Cli.cmds
