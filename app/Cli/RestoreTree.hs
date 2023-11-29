2c7339589c82bf87f988e2f7cb619d110dc504ab
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2023-11-29 19:29:09 +0800

    [exe] now 'restore tree' restore files concurrently

--- app/Cli/RestoreTree.hs
+++ app/Cli/RestoreTree.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Cli.RestoreTree (
@@ -9,6 +11,7 @@ module Cli.RestoreTree (
 import Control.Parallel (par)
 
 import Options.Applicative (
+  Alternative ((<|>)),
   ParserInfo,
   argument,
   help,
@@ -18,9 +21,14 @@ import Options.Applicative (
   progDesc,
  )
 
+import qualified Ki
+
+import Control.Concurrent.STM (STM, TBQueue, atomically, newTBQueueIO, readTBQueue, writeTBQueue)
 import Control.Exception (throwIO)
-import Control.Monad (unless)
+import Control.Monad (unless, void)
 import Control.Monad.IO.Class (MonadIO (liftIO))
+
+import Numeric.Natural (Natural)
 
 import Data.Foldable (Foldable (fold))
 import Data.Function ((&))
@@ -37,6 +45,7 @@ import qualified Effectful.Dispatch.Static.Unsafe as E
 import qualified System.Directory as D
 import System.IO (IOMode (WriteMode), withBinaryFile)
 
+import Path (Path)
 import qualified Path
 
 import Monad (run_readonly_repo_t_from_cwd)
@@ -86,14 +95,43 @@ parser_info = info (helper <*> parser) infoMod
       out_path_is_empty <- null <$> D.listDirectory (Path.fromAbsDir abs_out_path)
       unless out_path_is_empty $ throwIO $ userError $ "given directory is not empty: " <> Path.fromAbsDir abs_out_path
 
-      -- XXX Well, using name of Repo.Tree here is not very ideal since it breaks the meaning of name.
-      run_readonly_repo_t_from_cwd $ restore_tree $! Repo.Tree (T.pack $ Path.fromAbsDir abs_out_path) sha
+      run_readonly_repo_t_from_cwd $ do
+        mk_producer_and_workers
+          50
+          (S.maxBuffer 50 . S.eager True)
+          (\tbq -> traverse_tree_and_produce_jobs tbq abs_out_path sha)
+          ( \case
+              RestoreTree abs_dir_path digest -> restore_dir abs_dir_path digest
+              RestoreFile abs_file_path digest -> restore_file abs_file_path digest
+          )
 
-    -- TODO Copy from cat-file, should have only one copy.
+    {-# NOINLINE traverse_tree_and_produce_jobs #-}
+    traverse_tree_and_produce_jobs :: (Repository E.:> es, E.IOE E.:> es) => TBQueue RestoreJob -> Path Path.Abs Path.Dir -> Digest -> E.Eff es ()
+    traverse_tree_and_produce_jobs tbq abs_out_path tree_digest = do
+      liftIO $ do
+        D.createDirectoryIfMissing False $ Path.fromAbsDir abs_out_path
+        atomically $ writeTBQueue tbq $ RestoreTree abs_out_path tree_digest
+
+      E.reallyUnsafeUnliftIO $ \un ->
+        Repo.catTree tree_digest
+          & S.morphInner un
+          & S.mapM -- TODO parMapM like backup
+            ( either
+                ( \(Repo.Tree name digest) -> do
+                    rel_subtree_path <- Path.parseRelDir $ T.unpack name
+                    un $ traverse_tree_and_produce_jobs tbq (abs_out_path Path.</> rel_subtree_path) digest
+                )
+                ( \(Repo.FFile name digest) -> do
+                    rel_file_path <- Path.parseRelFile $ T.unpack name
+                    atomically $ writeTBQueue tbq $ RestoreFile (abs_out_path Path.</> rel_file_path) digest
+                )
+            )
+          & S.fold F.drain
+
     {-# NOINLINE restore_file #-}
-    restore_file :: (Repository E.:> es, E.IOE E.:> es) => Repo.FFile -> E.Eff es ()
-    restore_file f = E.reallyUnsafeUnliftIO $ \un -> withBinaryFile (T.unpack $ Repo.file_name f) WriteMode $ \h -> liftIO $ do
-      Repo.catFile (Repo.file_sha f)
+    restore_file :: (Repository E.:> es, E.IOE E.:> es) => Path Path.Abs Path.File -> Digest -> E.Eff es ()
+    restore_file abs_out_path digest = E.reallyUnsafeUnliftIO $ \un -> withBinaryFile (Path.fromAbsFile abs_out_path) WriteMode $ \h -> do
+      Repo.catFile digest
         & S.morphInner un
         -- Use parConcatMap to open multiple chunk files concurrently.
         -- This allow us to read from catFile and open chunk file ahead of time before catual writing.
@@ -103,15 +141,30 @@ parser_info = info (helper <*> parser) infoMod
         & S.parEval (S.maxBuffer 30)
         & S.fold (Handle.writeChunks h)
 
-    {-# NOINLINE restore_tree #-}
-    restore_tree :: (Repository E.:> es, E.IOE E.:> es) => Repo.Tree -> E.Eff es ()
-    restore_tree t = do
-      let tree_path = T.unpack $ Repo.tree_name t
+    {-# NOINLINE restore_dir #-}
+    restore_dir :: (Repository E.:> es, E.IOE E.:> es) => Path Path.Abs Path.Dir -> Digest -> E.Eff es ()
+    restore_dir _ _ = do
       -- TODO Permission should be backed up as well.
-      liftIO $ D.createDirectoryIfMissing False tree_path
-      E.reallyUnsafeUnliftIO $ \un ->
-        D.withCurrentDirectory tree_path $
-          un $
-            Repo.catTree (Repo.tree_sha t)
-              & S.mapM (either restore_tree restore_file)
-              & S.fold F.drain
+      pure ()
+
+data RestoreJob = RestoreTree (Path Path.Abs Path.Dir) Digest | RestoreFile (Path Path.Abs Path.File) Digest
+  deriving (Show)
+
+mk_producer_and_workers :: (E.IOE E.:> es) => Natural -> (S.Config -> S.Config) -> (TBQueue e -> E.Eff es a) -> (e -> E.Eff es ()) -> E.Eff es a
+mk_producer_and_workers n par_map_config emitter worker = E.reallyUnsafeUnliftIO $ \un -> do
+  tbq <- newTBQueueIO n
+
+  Ki.scoped $ \scope -> do
+    emitter_thread <- Ki.fork scope $ un $ emitter tbq
+    reciver_thread <- Ki.fork scope $ un $ reciver tbq worker (void $ Ki.await emitter_thread)
+    emitter_ret <- atomically $ Ki.await emitter_thread
+    () <- atomically $ Ki.await reciver_thread
+    pure emitter_ret
+  where
+    reciver :: (E.IOE E.:> es) => TBQueue e -> (e -> E.Eff es ()) -> STM () -> E.Eff es ()
+    reciver tbq' worker' emitter_stopped = E.reallyUnsafeUnliftIO $ \un -> do
+      S.unfoldrM
+        (\() -> atomically $ (Just . (,()) <$> readTBQueue tbq') <|> (Nothing <$ emitter_stopped))
+        ()
+        & S.parMapM par_map_config (un . worker')
+        & S.fold F.drain
