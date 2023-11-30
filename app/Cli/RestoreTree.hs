e4463944fcd76ebd657b6792a11d96dd64bbc0d0
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2023-11-30 13:32:31 +0800

    [exe] add new traversal way, BFS, to restore tree
    BFS runs faster but use a bit more memory, still a good default though.

--- app/Cli/RestoreTree.hs
+++ app/Cli/RestoreTree.hs
@@ -8,6 +8,7 @@ import Options.Applicative (
   Alternative ((<|>)),
   ParserInfo,
   argument,
+  flag,
   help,
   helper,
   info,
@@ -17,15 +18,16 @@ import Options.Applicative (
 
 import Ki qualified
 
-import Control.Concurrent.STM (STM, TBQueue, atomically, newTBQueueIO, readTBQueue, writeTBQueue)
-import Control.Exception (throwIO)
-import Control.Monad (unless, void)
+import Control.Concurrent.STM (STM, TBQueue, atomically, check, modifyTVar', newTBQueueIO, newTQueueIO, newTVarIO, readTBQueue, readTQueue, readTVar, writeTBQueue, writeTQueue)
+import Control.Exception (bracket_, throwIO)
+import Control.Monad (replicateM_, unless, void)
 import Control.Monad.IO.Class (MonadIO (liftIO))
 
 import Numeric.Natural (Natural)
 
 import Data.Foldable (Foldable (fold))
-import Data.Function ((&))
+import Data.Function (fix, (&))
+import Data.Word (Word32)
 
 import Data.Text qualified as T
 
@@ -58,7 +60,14 @@ parser_info = info (helper <*> parser) infoMod
 
     parser =
       go
-        <$> argument
+        <$> flag
+          BFS
+          DFS
+          ( fold
+              [ help "Use DFS instead of BFS to traverse backup tree while restoring."
+              ]
+          )
+        <*> argument
           digestRead
           ( fold
               [ metavar "SHA"
@@ -74,8 +83,8 @@ parser_info = info (helper <*> parser) infoMod
           )
 
     {-# NOINLINE go #-}
-    go :: Digest -> Path.SomeBase Path.Dir -> IO ()
-    go sha some_dir = do
+    go :: TraverseMethod -> Digest -> Path.SomeBase Path.Dir -> IO ()
+    go traverse_method sha some_dir = do
       abs_out_path <- case some_dir of
         Path.Abs abs_dir -> pure abs_dir
         Path.Rel rel_dir -> do
@@ -88,6 +97,11 @@ parser_info = info (helper <*> parser) infoMod
       out_path_is_empty <- null <$> D.listDirectory (Path.fromAbsDir abs_out_path)
       unless out_path_is_empty $ throwIO $ userError $ "given directory is not empty: " <> Path.fromAbsDir abs_out_path
 
+      let
+        traverse_tree_and_produce_jobs = case traverse_method of
+          BFS -> traverse_tree_and_produce_jobs_bfs
+          DFS -> traverse_tree_and_produce_jobs_dfs
+
       run_readonly_repo_t_from_cwd $ do
         mk_producer_and_workers
           50
@@ -98,28 +112,72 @@ parser_info = info (helper <*> parser) infoMod
               RestoreFile abs_file_path digest -> restoreFile abs_file_path digest
           )
 
-    {-# NOINLINE traverse_tree_and_produce_jobs #-}
-    traverse_tree_and_produce_jobs :: (Repository E.:> es, E.IOE E.:> es) => TBQueue RestoreJob -> Path Path.Abs Path.Dir -> Digest -> E.Eff es ()
-    traverse_tree_and_produce_jobs tbq abs_out_path tree_digest = do
-      liftIO $ do
-        D.createDirectoryIfMissing False $ Path.fromAbsDir abs_out_path
-        atomically $ writeTBQueue tbq $ RestoreTree abs_out_path tree_digest
+data TraverseMethod = DFS | BFS
 
-      E.reallyUnsafeUnliftIO $ \un ->
-        Repo.catTree tree_digest
-          & S.morphInner un
-          & S.mapM -- TODO parMapM like backup
-            ( either
-                ( \(Repo.Tree name digest) -> do
-                    rel_subtree_path <- Path.parseRelDir $ T.unpack name
-                    un $ traverse_tree_and_produce_jobs tbq (abs_out_path Path.</> rel_subtree_path) digest
-                )
-                ( \(Repo.FFile name digest) -> do
-                    rel_file_path <- Path.parseRelFile $ T.unpack name
-                    atomically $ writeTBQueue tbq $ RestoreFile (abs_out_path Path.</> rel_file_path) digest
-                )
+{-# NOINLINE traverse_tree_and_produce_jobs_dfs #-}
+traverse_tree_and_produce_jobs_dfs :: (Repository E.:> es, E.IOE E.:> es) => TBQueue RestoreJob -> Path Path.Abs Path.Dir -> Digest -> E.Eff es ()
+traverse_tree_and_produce_jobs_dfs tbq abs_out_path tree_digest = do
+  liftIO $ do
+    D.createDirectoryIfMissing False $ Path.fromAbsDir abs_out_path
+    atomically $ writeTBQueue tbq $ RestoreTree abs_out_path tree_digest
+
+  E.reallyUnsafeUnliftIO $ \un ->
+    Repo.catTree tree_digest
+      & S.morphInner un
+      & S.mapM -- TODO parMapM like backup
+        ( either
+            ( \(Repo.Tree name digest) -> do
+                rel_subtree_path <- Path.parseRelDir $ T.unpack name
+                un $ traverse_tree_and_produce_jobs_dfs tbq (abs_out_path Path.</> rel_subtree_path) digest
             )
-          & S.fold F.drain
+            ( \(Repo.FFile name digest) -> do
+                rel_file_path <- Path.parseRelFile $ T.unpack name
+                atomically $ writeTBQueue tbq $ RestoreFile (abs_out_path Path.</> rel_file_path) digest
+            )
+        )
+      & S.fold F.drain
+
+{-# NOINLINE traverse_tree_and_produce_jobs_bfs #-}
+traverse_tree_and_produce_jobs_bfs :: (Repository E.:> es, E.IOE E.:> es) => TBQueue RestoreJob -> Path Path.Abs Path.Dir -> Digest -> E.Eff es ()
+traverse_tree_and_produce_jobs_bfs restore_job_tbq abs_root_path root_digest = do
+  E.reallyUnsafeUnliftIO $ \un -> Ki.scoped $ \scope -> do
+    tree_digest_tq <- newTQueueIO
+    atomically $ writeTQueue tree_digest_tq (abs_root_path, root_digest)
+
+    num_of_working_thread_tvar <- newTVarIO (0 :: Word32)
+    let
+      take_job = modifyTVar' num_of_working_thread_tvar succ
+      done_job = modifyTVar' num_of_working_thread_tvar pred
+      running_bracket = bracket_ (pure ()) (atomically done_job)
+      no_one_is_running = check . (0 ==) =<< readTVar num_of_working_thread_tvar
+
+    replicateM_ 4 {- CORE/2 -} $ Ki.fork scope $ fix $ \rec -> do
+      opt_digest <- atomically $ do
+        (Just <$> (take_job >> readTQueue tree_digest_tq)) <|> (Nothing <$ no_one_is_running)
+
+      case opt_digest of
+        Just (parent_abs_path, parent_digest) -> do
+          running_bracket $ do
+            D.createDirectoryIfMissing False $ Path.fromAbsDir parent_abs_path
+            atomically $ writeTBQueue restore_job_tbq $ RestoreTree parent_abs_path parent_digest
+
+            un $
+              Repo.catTree parent_digest
+                & S.mapM
+                  ( either
+                      ( \(Repo.Tree name child_digest) -> do
+                          rel_subtree_path <- Path.parseRelDir $ T.unpack name
+                          liftIO $ atomically $ writeTQueue tree_digest_tq (parent_abs_path Path.</> rel_subtree_path, child_digest)
+                      )
+                      ( \(Repo.FFile name digest) -> do
+                          rel_file_path <- Path.parseRelFile $ T.unpack name
+                          liftIO $ atomically $ writeTBQueue restore_job_tbq $ RestoreFile (parent_abs_path Path.</> rel_file_path) digest
+                      )
+                  )
+                & S.fold F.drain
+          rec
+        Nothing -> pure ()
+    atomically $ Ki.awaitAll scope
 
 data RestoreJob = RestoreTree (Path Path.Abs Path.Dir) Digest | RestoreFile (Path Path.Abs Path.File) Digest
   deriving (Show)
