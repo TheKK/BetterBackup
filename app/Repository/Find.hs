eaa6d97109482cafe64bef2145e1959897793bcd
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2023-12-04 19:29:38 +0800

    [exe] introduce searchFileInTree & searchItemInTree to Repository.Find

--- app/Repository/Find.hs
+++ app/Repository/Find.hs
@@ -3,15 +3,18 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE TypeOperators #-}
 
 module Repository.Find (
   findTree,
   searchDirInTree,
+  searchFileInTree,
+  searchItemInTree,
 ) where
 
 import Control.Monad (when, (<$!>), (<=<))
 import Control.Monad.IO.Class (MonadIO (liftIO))
+import Control.Monad.Trans (lift)
+import Control.Monad.Trans.Except (ExceptT, except, runExceptT, throwE)
 
 import Streamly.Data.Fold qualified as F
 import Streamly.Data.Stream qualified as S
@@ -30,11 +33,9 @@ import Path qualified
 
 import Effectful qualified as E
 
-import Better.Hash (TreeDigest)
+import Better.Hash (FileDigest, TreeDigest)
 import Better.Repository qualified as Repo
 import Better.Repository.Class (Repository)
-import Control.Monad.Trans (lift)
-import Control.Monad.Trans.Except (ExceptT, except, runExceptT)
 
 findTree :: (Repository E.:> es, E.IOE E.:> es) => Bool -> Maybe Word64 -> TreeDigest -> Maybe (Path.SomeBase Path.Dir) -> E.Eff es ()
 findTree show_digest opt_depth tree_root opt_somebase_dir = do
@@ -62,38 +63,61 @@ findTree show_digest opt_depth tree_root opt_somebase_dir = do
             )
           & S.fold F.drain
 
+searchFileInTree :: (Repository E.:> es) => TreeDigest -> Path.SomeBase Path.File -> E.Eff es (Either String FileDigest)
+searchFileInTree root_digest somebase =
+  searchItemInTree root_digest (Path.fromAbsFile abs_file_path) >>= \case
+    Left err_msg -> error err_msg
+    Right (Left tree_digest) -> error $ "'" <> Path.toFilePath abs_file_path <> " [" <> show tree_digest <> "] is not an file but directory"
+    Right (Right file_digest) -> pure $! Right $! file_digest
+  where
+    abs_file_path :: Path Path.Abs Path.File
+    abs_file_path = case somebase of
+      Path.Abs abs_file -> abs_file
+      Path.Rel rel_file -> [Path.absdir|/|] Path.</> rel_file
+
 searchDirInTree :: (Repository E.:> es) => TreeDigest -> Path.SomeBase Path.Dir -> E.Eff es (Either String TreeDigest)
-searchDirInTree root_digest somebase = do
-  -- Make everything abs dir, then split them into ["/", "a", "b"] and drop the "/" part.
-  let dir_segments = tail $ FP.splitDirectories $ Path.fromAbsDir abs_dir_path
-  go root_digest dir_segments
+searchDirInTree root_digest somebase =
+  searchItemInTree root_digest (Path.fromAbsDir abs_dir_path) >>= \case
+    Left err_msg -> error err_msg
+    Right (Left tree_digest) -> pure $! Right $! tree_digest
+    Right (Right file_digest) -> error $ "'" <> Path.toFilePath abs_dir_path <> " [" <> show file_digest <> "] is not an directory but file"
   where
     abs_dir_path :: Path Path.Abs Path.Dir
     abs_dir_path = case somebase of
       Path.Abs abs_dir -> abs_dir
       Path.Rel rel_dir -> [Path.absdir|/|] Path.</> rel_dir
 
-    go :: (Repository E.:> es) => TreeDigest -> [FilePath] -> E.Eff es (Either String TreeDigest)
-    go !digest path_segments = runExceptT $ snd <$> foldlM step (["/"], digest) path_segments
+searchItemInTree :: (Repository E.:> es) => TreeDigest -> FilePath -> E.Eff es (Either String (Either TreeDigest FileDigest))
+searchItemInTree !root_digest path = runExceptT $ snd <$> foldlM step (["/"], Left root_digest) path_segments
+  where
+    -- Make everything abs dir, then split them into ["/", "a", "b"] and drop the "/" part.
+    path_segments = filter (/= "/") $ FP.splitDirectories path
 
     {-# INLINE step #-}
-    step :: (Repository E.:> es) => ([FilePath], TreeDigest) -> FilePath -> ExceptT String (E.Eff es) ([FilePath], TreeDigest)
-    step (traversed_pathes, !digest) path = do
+    step :: (Repository E.:> es) => ([FilePath], Either TreeDigest FileDigest) -> FilePath -> ExceptT String (E.Eff es) ([FilePath], Either TreeDigest FileDigest)
+    step (traversed_pathes, !tree_or_file_digest) path_segment = do
+      !digest <- case tree_or_file_digest of
+        Left tree_digest -> pure tree_digest
+        Right file_digest -> throwE $ "'" <> FP.joinPath traversed_pathes <> "' [" <> show file_digest <> "] is not an directory but file"
+
       !next_digest <-
         (except <=< lift) $
           Repo.catTree digest
             & S.mapMaybe
               ( \case
                   Left tree ->
-                    if Repo.tree_name tree == T.pack path
-                      then Just $! Repo.tree_sha tree
+                    if Repo.tree_name tree == T.pack path_segment
+                      then Just $! Left $! Repo.tree_sha tree
                       else Nothing
-                  Right _file -> Nothing
+                  Right file ->
+                    if Repo.file_name file == T.pack path_segment
+                      then Just $! Right $! Repo.file_sha file
+                      else Nothing
               )
             & S.fold F.one
             & fmap
               ( \case
-                  Nothing -> Left $ "unable to find directory '" <> path <> "' under '" <> FP.joinPath traversed_pathes <> "' [" <> show digest <> "] in backup tree"
+                  Nothing -> Left $ "unable to find directory '" <> path_segment <> "' under '" <> FP.joinPath traversed_pathes <> "' [" <> show digest <> "] in backup tree"
                   Just d -> Right d
               )
-      pure (traversed_pathes <> [path], next_digest)
+      pure (traversed_pathes <> [path_segment], next_digest)
