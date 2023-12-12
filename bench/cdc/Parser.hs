b6659440ec5997101e9c94624ce94f7d6bf4acba
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2023-12-13 00:03:44 +0800

    [bench] fix compile error after function renaming

--- bench/cdc/Parser.hs
+++ bench/cdc/Parser.hs
@@ -6,50 +6,41 @@ module Parser (
   benchGroup,
 ) where
 
-import Criterion.Main (bench, bgroup, defaultMain, env, nf, nfAppIO, whnf, whnfAppIO)
+import Criterion.Main (bench, bgroup, whnf, whnfAppIO)
 import Criterion.Types (Benchmark)
 
-import Control.Exception (displayException, mask_)
-import Control.Monad (forever)
-import Control.Monad.IO.Class (MonadIO (liftIO))
+import Control.Applicative ((<|>))
+import Control.Exception (displayException)
 
-import Data.Char
-import Data.Foldable (Foldable (fold))
+import Data.ByteString qualified as BS
+import Data.ByteString.Builder qualified as BB
+import Data.ByteString.Char8 qualified as BC
+import Data.ByteString.Lazy qualified as BL
+import Data.ByteString.Short qualified as BSS
+import Data.Char (chr, isAlphaNum)
 import Data.Function ((&))
 import Data.Functor.Identity (Identity (runIdentity))
+import Data.Void (Void)
+import Data.Word (Word8)
 
-import qualified Streamly.Console.Stdio as Stdio
-import qualified Streamly.Data.Fold as F
-import qualified Streamly.Data.Parser as Par
-import qualified Streamly.Data.Stream as S
-import qualified Streamly.Data.Stream.Prelude as S
-import qualified Streamly.Internal.Data.Parser as Par
+import Streamly.Data.Fold qualified as F
+import Streamly.Data.Parser qualified as Par
+import Streamly.Data.Stream qualified as S
+import Streamly.Internal.Data.Parser qualified as Par
 
 import Path (Path)
-import qualified Path
+import Path qualified
 
-import qualified Better.Repository.Backup as Repo
-
-import Better.Hash (Digest, digestFromByteString)
-import qualified Better.Internal.Repository.LowLevel as Repo
-import qualified Better.Statistics.Backup as BackupSt
-import Better.Statistics.Backup.Class (BackupStatistics, newChunkCount, newDirCount, newFileCount, processedChunkCount, processedDirCount, processedFileCount, totalDirCount, totalFileCount, uploadedBytes)
-import Control.Applicative ((<|>))
-import Control.Concurrent (threadDelay)
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BC
-import qualified Data.ByteString.Builder as BB
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Short as BSS
-import Data.Word (Word8)
 import Streamly.External.ByteString (fromArray, toArray)
-import qualified Streamly.Internal.Data.Stream.Chunked as Chunked
+import Streamly.Internal.Data.Stream.Chunked qualified as Chunked
 
 import Text.Megaparsec (Parsec)
-import qualified Text.Megaparsec as Mega
-import qualified Text.Megaparsec.Byte as Byte
-import qualified Text.Megaparsec.Byte.Lexer as Lex
+import Text.Megaparsec qualified as Mega
+import Text.Megaparsec.Byte qualified as Byte
+import Text.Megaparsec.Byte.Lexer qualified as Lex
+
+import Better.Data.FileSystemChanges (FileSystemChange, FileSystemChanges)
+import Better.Data.FileSystemChanges qualified as FSC
 
 benchGroup :: Benchmark
 benchGroup =
@@ -69,8 +60,9 @@ megaparsec_parser_banch num =
   bench ("megaparsec-parse-then-fold-" <> show num) $
     whnfAppIO
       ( \input ->
-            & fmap fromArray
+          S.fromList input
             & S.filter (not . BS.null)
+            & fmap toArray
             & Chunked.splitOn (fromIntegral $ fromEnum '\n')
             & fmap fromArray
             & S.filter (not . BS.null)
@@ -84,8 +76,9 @@ megaparsec_parser_banch' =
   bench "megaparsec-parse" $
     whnfAppIO
       ( \input ->
-            & fmap fromArray
+          S.fromList input
             & S.filter (not . BS.null)
+            & fmap toArray
             & Chunked.splitOn (fromIntegral $ fromEnum '\n')
             & fmap fromArray
             & S.filter (not . BS.null)
@@ -95,8 +88,8 @@ megaparsec_parser_banch' =
       (_input 10000)
 
 {-# INLINE the_fold #-}
-the_fold :: Monad m => F.Fold m (Path Path.Rel Path.Dir, Repo.FileSystemChange) Repo.FileSystemChanges
-the_fold = F.foldl' (flip $ uncurry Repo.insertFileSystemChange) Repo.emptyFileSystemChanges
+the_fold :: Monad m => F.Fold m (Path Path.Rel Path.Dir, FileSystemChange) FileSystemChanges
+the_fold = F.foldl' (flip $ uncurry FSC.insert') FSC.empty
 
 lexeme :: MegaParser a -> MegaParser a
 lexeme = Lex.lexeme Byte.space1
@@ -124,15 +117,15 @@ to_abs_file p = do
     Left e -> fail $ displayException e
     Right path -> pure path
 
-parser :: MegaParser (Path Path.Rel Path.Dir, Repo.FileSystemChange)
+parser :: MegaParser (Path Path.Rel Path.Dir, FileSystemChange)
 parser = do
   rel_path <- to_rel_dir $ lexeme filepath_p
   !change <- (removeP <|> newP <|> freshP)
   pure (rel_path, change)
   where
-    removeP = Repo.IsRemoved <$ keyword' "remove"
-    newP = Repo.IsNew <$ keyword "new" <*> to_abs_file filepath_p
-    freshP = Repo.NeedFreshBackup <$ keyword "fresh" <*> to_abs_file filepath_p
+    removeP = FSC.IsRemoved <$ keyword' "remove"
+    newP = FSC.IsNew <$ keyword "new" <*> to_abs_file filepath_p
+    freshP = FSC.NeedFreshBackup <$ keyword "fresh" <*> to_abs_file filepath_p
 
 running = Mega.parse parser "src"
 
@@ -151,8 +144,8 @@ streamly_parser_bench =
       (_input 10000)
   where
     {-# INLINE the_fold #-}
-    the_fold :: Monad m => F.Fold m (Path Path.Rel Path.Dir, Repo.FileSystemChange) Repo.FileSystemChanges
-    the_fold = F.foldl' (flip $ uncurry Repo.insertFileSystemChange) Repo.emptyFileSystemChanges
+    the_fold :: Monad m => F.Fold m (Path Path.Rel Path.Dir, FileSystemChange) FileSystemChanges
+    the_fold = F.foldl' (flip $ uncurry FSC.insert') FSC.empty
 
     {-# INLINE lexeme #-}
     lexeme :: (Monad m) => Par.Parser Word8 m b -> Par.Parser Word8 m b
@@ -190,21 +183,21 @@ streamly_parser_bench =
         Right path -> pure path
 
     {-# INLINE parser #-}
-    parser :: (Monad m) => Par.Parser Word8 m (Path Path.Rel Path.Dir, Repo.FileSystemChange)
+    parser :: (Monad m) => Par.Parser Word8 m (Path Path.Rel Path.Dir, FileSystemChange)
     parser = do
       rel_path <- to_rel_dir filepath_p
       !change <- removeP <|> newP <|> freshP
       pure (rel_path, change)
       where
         {-# INLINE removeP #-}
-        removeP = Repo.IsRemoved <$ keyword "remove"
+        removeP = FSC.IsRemoved <$ keyword "remove"
         {-# INLINE newP #-}
-        newP = Repo.IsNew <$ keyword "new" <*> to_abs_file filepath_p
+        newP = FSC.IsNew <$ keyword "new" <*> to_abs_file filepath_p
         {-# INLINE freshP #-}
-        freshP = Repo.NeedFreshBackup <$ keyword "fresh" <*> to_abs_file filepath_p
+        freshP = FSC.NeedFreshBackup <$ keyword "fresh" <*> to_abs_file filepath_p
 
     {-# INLINE parse_changes #-}
-    parse_changes :: (Monad m) => S.Stream m Word8 -> S.Stream m (Either Par.ParseError (Path Path.Rel Path.Dir, Repo.FileSystemChange))
+    parse_changes :: (Monad m) => S.Stream m Word8 -> S.Stream m (Either Par.ParseError (Path Path.Rel Path.Dir, FileSystemChange))
     parse_changes = S.parseMany parser . S.dropWhile (\c -> c == fromIntegral (fromEnum ' ') || c == fromIntegral (fromEnum '\n'))
 
 {-# NOINLINE _input #-}
