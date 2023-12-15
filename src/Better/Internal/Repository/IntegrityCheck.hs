228050c6388ff48f574ad889830e4e509c9eae38
Ying-Ruei Liang (TheKK) <thumbd03803@gmail.com>
2023-12-16 03:57:17 +0800

    [lib] update impl of checksum to support all encryption

--- src/Better/Internal/Repository/IntegrityCheck.hs
+++ src/Better/Internal/Repository/IntegrityCheck.hs
@@ -43,7 +43,6 @@ import Better.Repository.Class qualified as E
 
 import Control.Monad.IO.Class (liftIO)
 
-import Data.Coerce (coerce)
 import Data.Maybe (fromJust, isJust)
 
 import Better.Internal.Repository.LowLevel qualified as Repo
@@ -51,48 +50,18 @@ import Better.Internal.Streamly.Crypto.AES (decryptCtr)
 
 checksum :: (E.Repository E.:> es) => Int -> E.Eff es ()
 checksum n = do
-  -- TODO Use traverse & worker pipeline to reuse worker.
-  check_encrypted n folder_version
-  check_plaintext n folder_tree
-  check_encrypted n folder_file
-  check_encrypted n folder_chunk
-
-check_plaintext :: (E.Repository E.:> es) => Int -> Path.Path Path.Rel Path.Dir -> E.Eff es ()
-check_plaintext n folder = E.unsafeConcUnliftIO E.Ephemeral E.Unlimited $ \un -> do
-  listFolderFiles folder
-    & S.morphInner un
-    & S.parMapM
-      (S.maxBuffer (n + 1) . S.eager True)
-      ( \chunk_path -> do
-          expected_sha <- s2d $ Path.fromRelFile chunk_path
-          actual_sha <-
-            Repo.read (folder </> chunk_path)
-              & S.morphInner un
-              & S.fold hashArrayFoldIO
-          if coerce expected_sha == actual_sha
-            then pure Nothing
-            else
-              let
-                !actual_sha_str = T.pack $ show actual_sha
-              in
-                pure $ Just (folder </> chunk_path, actual_sha_str)
-      )
-    & S.filter isJust
-    & fmap fromJust
-    & S.fold
-      ( F.foldMapM $ \(invalid_f, actual_sha) ->
-          liftIO $ T.putStrLn $ "invalid file: " <> T.pack (Path.toFilePath invalid_f) <> ", checksum: " <> actual_sha
-      )
-
-check_encrypted :: (E.Repository E.:> es) => Int -> Path.Path Path.Rel Path.Dir -> E.Eff es ()
-check_encrypted n folder = do
   aes <- Repo.getAES
   E.unsafeConcUnliftIO E.Ephemeral E.Unlimited $ \un -> do
-    listFolderFiles folder
+    S.fromList [folder_version, folder_tree, folder_file, folder_chunk]
+      & S.concatMap
+        ( \p ->
+            listFolderFiles p
+              & fmap (\f -> (p, f))
+        )
       & S.morphInner un
       & S.parMapM
         (S.maxBuffer (n + 1) . S.eager True)
-        ( \chunk_path -> do
+        ( \(folder, chunk_path) -> do
             expected_sha <- s2d $ Path.fromRelFile chunk_path
             actual_sha <-
               Repo.read (folder </> chunk_path)
